c00bd3c4cf6178d4fd4bc58261c611bf6cdec5bc
edouard <edouard.malot@gmail.com>
2016-11-15 12:06:15 +0100

    Circl App first commit

--- Circl/src/main/java/io/circl/circl/MapsActivity.java
+++ Circl/src/main/java/io/circl/circl/MapsActivity.java
@@ -345,6 +345,7 @@ public class MapsActivity extends FragmentActivity  implements OnMapReadyCallbac
     }
 
 
+
     //////////////////////////////////////////////////
     //                                              //
     //                                              //
